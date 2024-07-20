598fc0a012a06670d62f153cd2eae260a434b863
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-20 10:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	//write your code here...
+    let prices = document.getElementsByClassName("price"); // use the correct function name
+    let sum = 0;
+    for(let i = 0; i < prices.length; i++) {
+        sum += Number(prices[i].innerText); // convert the innerText to a number and add it to the sum
+    }
+    console.log(sum);
 };
 
 getSumBtn.addEventListener("click", getSum);
